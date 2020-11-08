cb9db4a4673e9b194b1530a834ea538933c8fc42
alekskositsyn <61062623+alekskositsyn@users.noreply.github.com>
2020-11-08 13:04:53 +0300

    -изменил шаблонизатор -создал базовый шаблон

--- swallow/templator.py
+++ swallow/templator.py
@@ -1,8 +1,9 @@
-import os
-from jinja2 import Template
+""" Шаблонизатор. """
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
-def templates_engine(template_name, folder='templates', **kwargs):
+def templates_engine(template_name, folder='../templates', **kwargs):
     """
     Шаблонизатор - осуществляет рендер шаблонов.
     :param template_name: имя шаблона
@@ -10,8 +11,14 @@ def templates_engine(template_name, folder='templates', **kwargs):
     :param kwargs: параметры для передачи в шаблон
     :return:
     """
-    path = os.path.join(folder, template_name)
-    with open(path, encoding='utf-8') as html:
-        template = Template(html.read())
+    # folder_path = os.path.join(folder, template_name)
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
     return template.render(**kwargs)
 
+
+if __name__ == '__main__':
+    # Пример использования
+    output_test = templates_engine('contacts.html')
+    print(output_test)
